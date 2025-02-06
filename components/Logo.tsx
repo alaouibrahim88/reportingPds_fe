b4fc7c2b691a67fc1c1752fb58e707838bfcd89a
BasicHealin <hamidbounsir@outlook.fr>
2025-02-06 01:24:41 +0100

    add more details to scrap and change the logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { SquareDashedMousePointer } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 function Logo({
   fontSize = "text-2xl",
@@ -18,14 +19,14 @@ function Logo({
         fontSize
       )}
     >
-      <div className="rounded-xl bg-gradient-to-r from-primary to-primary/80 p-2">
-        <SquareDashedMousePointer size={iconSize} className="stroke-white" />
-      </div>
-      <div>
-        <span className="bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent">
-          Poly
-        </span>
-        <span className="text-foreground">design</span>
+      <div className="rounded-xl  p-2">
+        <Image
+          src="/polydesignlogo.webp"
+          alt="logo"
+          width={160}
+          height={160}
+          priority
+        />
       </div>
     </Link>
   );
