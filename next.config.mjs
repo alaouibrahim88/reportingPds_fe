b566d798f6c2cf6ac05da65f588b2631288b7361
Brahim El jaddioui <Brahim.Eljaddioui@polydesignsystems.com>
2026-03-08 08:26:57 +0000

    fix: add rewrites for API proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/Polydesign/Kpi/:path*",
+        destination: "http://127.0.0.1:2222/api/Polydesign/Kpi/:path*"
+      },
+      {
+        source: "/api/Polydesign/Reporting/:path*",
+        destination: "http://127.0.0.1:2222/api/Polydesign/Reporting/:path*"
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:2222/api/:path*"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
