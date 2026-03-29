482dda5de2b6c543569ed6d4bebbf1547242a04a
bader ben-lhachemi <baderbenlhachemi@gmail.com>
2026-03-29 22:10:49 +0100

    fix(layout): stabilize page positioning after viewport resize

--- app/(dashboard)/scrap/page.tsx
+++ app/(dashboard)/scrap/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/(dashboard)/scrap/_components/Container";
 
 export default function Home() {
   return (
-    <div className="h-full w-full flex justify-center bg-muted/40">
+    <div className="w-full flex justify-center bg-muted/40">
       <Container />
     </div>
   );
